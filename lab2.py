f9c5483ae97768f85c036b802dad1077c9475a23
sandeep <90399591+sandeepmsv@users.noreply.github.com>
2022-01-29 20:02:42 +0530

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,3 +1,13 @@
+''' Q. Write a program to perform two function: 
+a. Sum of square
+b. Sleep function
+
+
+- Perform it using sequential approach
+- Perform it using multithread approach
+- Calculate and Print the time taken by both to execute. '''
+
+
 from threading import *
 import time
 class MyThread():
